3b61e822896080174cb776dc411db7831a9d0dec
sonny <sonny@linecorp.com>
2018-09-06 09:32:05 +0900

    Initial Xcode project blank

---
+++ IssuesTracker/ViewController.swift
@@ -0,0 +1,21 @@
+//
+// ViewController.swift  
+// IssuesTracker
+// Created by Sonny on 2018/09/06.
+// Copyright © 2018 KyushuCoder. All rights reserved. 
+// All you need in life is love and laughter.
+// 
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ IssuesTrackerTests/IssuesTrackerTests.swift
@@ -0,0 +1,35 @@
+//
+// IssuesTrackerTests.swift  
+// IssuesTracker
+// Created by Sonny on 2018/09/06.
+// Copyright © 2018 KyushuCoder. All rights reserved. 
+// All you need in life is love and laughter.
+// 
+
+
+import XCTest
+@testable import IssuesTracker
+
+class IssuesTrackerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
